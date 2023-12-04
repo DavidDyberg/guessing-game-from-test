616803ba57b47fcc19b5b05be5e3bc1cdb0c7c5a
DavidDyberg <david.dyberg@edu.cmeducations.se>
2023-12-04 09:42:16 +0100

    add hover effect on buttons

--- src/components/color.jsx
+++ src/components/color.jsx
@@ -4,7 +4,7 @@ const ColorButton = ({ color, handleClick }) => {
   const bgColor = "#" + color;
 
   return (
-    <button className="rounded-md px-3 py-2 md:m-2 my-1 bg-slate-800 text-white"
+    <button className="rounded-md px-3 py-2 md:m-2 my-1 bg-slate-800 text-white hover:bg-slate-700 cursor-pointer"
       onClick={() => handleClick(color)}>
       {color}
     </button>
